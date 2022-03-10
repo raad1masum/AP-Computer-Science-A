1bdb5b3f941a8c4c6bf5777fd6d47b05d6aae1bd
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 10:26:08 -0800

    create test new class

---
+++ TestNew.java
@@ -0,0 +1,9 @@
+public class TestNew extends Blueprint {
+    public TestNew(String optionsText) {
+        super(optionsText);
+    }
+
+    public void run() {
+        System.out.println("running test new");
+    }
+}
