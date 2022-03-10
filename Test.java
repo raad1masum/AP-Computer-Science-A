5b48f587adb2b8caf2d3cf3988ec79d27611df73
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 10:14:26 -0800

    create test class

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public void run() {
+        System.out.println("running bruh");
+    }
+
+    @Override
+    public String toString() {
+        return "Test Option";
+    }
+}
