ab3188fc631442e825ce34b3028314eb171d4efa
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 10:22:42 -0800

    create a test class which extends runner

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test extends Runner {
+    public Test(String optionsText) {
+        super(optionsText);
+    }
+    
+    public void run() {
+        System.out.println("running test");
+    }
+}
