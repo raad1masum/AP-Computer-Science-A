bd309fc9e96fbd0bbcf5320a899d2d69390f31f5
Raadwan Masum <piraadwan@gmail.com>
2022-03-10 10:29:05 -0800

    clean up main

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 /*
-Creator: Nighthawk Coding Society
-Mini Lab Name: Hello Series, featuring Monkey Jumpers
-Level: Easy
+Author: Raadwan Masum
 */
 
 /* 
@@ -38,12 +36,14 @@ public class Main {
         options.put(2, new TestNew("Option 2: Test New Option"));
 
         while (true) {
-            menu("Menu", options); // 'coder' defined method/function call to a different code block
+            menu("Menu", options);
         }
     }
 
     /**
-     * menu - method that is activated by main, this will perform Java code
+     * print and run menu options
+     * @param banner dispay banner
+     * @param options HashMap of menu options
      */
     public static void menu(String banner, HashMap<Integer, Blueprint> options) {
         System.out.println(banner);
@@ -51,7 +51,6 @@ public class Main {
         
         for (int i = 1; i <= options.size(); i++) {
             System.out.println(i + ": " + options.get(i).toString());
-            // System.out.println(i + ": " + options.get("Option " + i));
         }
 
         System.out.print("Option> ");
@@ -59,95 +58,6 @@ public class Main {
         options.get(selection).run();
 
         scanner.close();
-        // System.out.println(options.get(selection).toString());
-        /*
-        // Primitive types: AP CSA Unit 1
-        int selection; // user selection variable of type Inteteger
-
-        // Using a Class: AP CSA Unit 2
-        // Scanner is well know Java class for text based input
-        // defining an object to scan/get input from user, notice the use of
-
-        Scanner scan = new Scanner(System.in);                                                // "new", this means you are making an object of type Scanner.
-
-        // System.out.println - A Class or Static method call that is used to output a
-        // message to the terminal.
-        // The block of outputs below is used to present a menu to the user.
-        System.out.println("-------------------------\n");
-        System.out.println("Choose from these choices");
-        System.out.println("-------------------------\n");
-        System.out.println("1 - Hello");
-        System.out.println("2 - Printy");
-        System.out.println("3 - Loopy");
-        System.out.println("4 - Classy Print");
-        System.out.println("5 - Classy Poem");
-        System.out.println("6 - Binary");
-        System.out.println("7 - Termy");
-        System.out.println("0 - Quit");
-
-        // Using Scanner Class to get integer as input
-        selection = scan.nextInt(); // Using nextInt() method to get an integer value
-
-        // Control Statements: AP CSA Unit 3
-        // Switch or Switch/Case is used to evaluate the input and execute the case that
-        // matches the selection.
-        switch (selection) {
-            case 0:
-                return;
-            case 1:
-                // Prints message to console/terminal
-                System.out.println("Hello, World of Monkeys!");
-                break;
-            case 2:
-                // Using a Class: AP CSA Unit 2
-                // Run Printy main method with null parameter
-                // Printy.main(null);
-                break;
-            case 3:
-                // Using a Class: AP CSA Unit 2
-                // Run MonkeyLoop() constructor and run a method from temporary object, defined
-                // in class
-                // new MonkeyLoop().printPoem();
-                break;
-            case 4:
-                // Using a Class: AP CSA Unit 2
-                // Run MonkeyList() constructor and run a method from temporary object, defined
-                // in class
-                // new MonkeyList().print();
-                break;
-            case 5:
-                // Using a Class: AP CSA Unit 2
-                // Create object to run methods from Classy.java
-                // MonkeyList ml = new MonkeyList();
-                // ml.print();
-                // ml.printPoem();
-                break;
-
-            case 6:
-                // Using a Class: AP CSA Unit 2
-                // Run Biny main method with null parameter
-                // Biny.main(null);
-                break;
-
-            case 7:
-                // Run Termy main method with null parameter
-                // Termy.main(null);
-                break;
-
-            default:
-                // Prints error message from console
-                System.out.println("Unexpected choice, try again.");
-        }
-
-        // Recursion: AP CSA requirement 10
-        // Calling the menu() method inside of the menu() code block is called
-        // recursion.
-        // Using recursion we provide the ability to stay in program and select and
-        // additional option.
-        // Repeating an operation is often called iteration. Recursion is a special form
-        // of iteration.
-        menu();
-        */
     }
 
 }
