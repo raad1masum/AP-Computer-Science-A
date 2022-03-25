2e0aed29e084afa07dabed52104e0f8cbc90707c
Raadwan Masum <piraadwan@gmail.com>
2022-03-25 02:21:26 -0700

    create stack class

---
+++ src/Stack.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Stack<T> {
+    private ArrayList<T> list = new ArrayList<T>();
+
+    public Stack() {
+    }
+
+    public void add(T info) {
+        list.add(info);
+    }
+
+    public void delete() {
+        if (!list.isEmpty())
+            list.remove(list.size() - 1);
+        else
+            System.out.println("null");
+    }
+
+    public T peak() {
+        if (!list.isEmpty())
+            return list.get(list.size() - 1);
+        else
+            return null;
+    }
+
+    public int length() {
+        return list.size();
+    }
+
+    public void clear() {
+        list.clear();
+    }
+
+    public void display() {
+        System.out.println(list);
+    }
+
+}
