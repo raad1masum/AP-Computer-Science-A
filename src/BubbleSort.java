69b0209159a5d86495263d069b59cec752ca1e1f
Raadwan Masum <piraadwan@gmail.com>
2022-04-08 09:55:52 -0700

    implement bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.util.ArrayList;
+
+public class BubbleSort extends BlueprintSort {
+    ArrayList<Integer> data = new ArrayList<Integer>();
+
+    public BubbleSort(String optionsText) {
+        super(optionsText);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5000; i++)
+            data.add((int) (Math.random() * 50000));
+
+        for (int i = 0; i < data.size(); i++)
+            System.out.print(data.get(i) + " ");
+        System.out.println();
+
+        boolean sorted = true;
+        for (int i = 1; i < data.size(); i++) {
+            for (int j = 1; j < data.size(); j++) {
+                if (data.get(j) < data.get(j - 1)) {
+                    sorted = false;
+                    int tmp = data.get(j - 1);
+                    data.set(j - 1, data.get(j));
+                    data.set(j, tmp);
+                }
+            }
+            if (sorted)
+                break;
+        }
+
+        for (int i = 0; i < data.size(); i++)
+            System.out.print(data.get(i) + " ");
+        System.out.println();
+
+    }
+}
